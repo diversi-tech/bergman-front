d4acd291f045277cc16c6ae5321a139483630804
tehila azani <166302730+tehilaAzani@users.noreply.github.com>
2024-06-25 08:53:21 +0300

    first comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Filter } from './components/filtering';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header><Image /></header>
+      <Filter></Filter>
+    </div>
+    
+  );
+}
+
+
+const תמונה = require('./images/logo small.jpg');
+
+function Image() {
+  return (
+    <div>
+      <img src={תמונה} alt="תיאור תמונה" className='image' />
     </div>
   );
 }
