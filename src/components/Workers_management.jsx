190f97675dd9a82cc43822165b1b7c2a6aac4ebf
Sari <sarag40404@gmail.com>
2024-07-07 17:22:06 +0300

    good

--- src/components/Workers_management.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export const Workers_management=()=>{
-    return<p>ניהול עובדים</p>
-}
--- src/components/upload_cv.jsx
+++
@@ -1,7 +0,0 @@
-export default function Upload_cv() {
-    return(
-        <div>
-            <h1>כניסה</h1>
-        </div>
-    )
-}
