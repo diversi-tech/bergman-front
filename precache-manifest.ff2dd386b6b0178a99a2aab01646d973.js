a8f03a7833b98a182b91aee36a7f9cb5e72ad292
Rivki-Hayas <r0534164722@gmail.com>
2024-07-11 08:43:55 +0300

    Updates

---
+++ precache-manifest.ff2dd386b6b0178a99a2aab01646d973.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1258ebf7a2c3517b8012bd40ec747ca0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "414ba2bf99c6b40e600c",
+    "url": "/static/css/main.d9d4ff13.chunk.css"
+  },
+  {
+    "revision": "b71b5f2af86df227e9ca",
+    "url": "/static/js/2.50e97de0.chunk.js"
+  },
+  {
+    "revision": "9b8348550cb4478e2bca3a2bae2b0094",
+    "url": "/static/js/2.50e97de0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e36f2f69405005e853",
+    "url": "/static/js/3.6dcb56ab.chunk.js"
+  },
+  {
+    "revision": "414ba2bf99c6b40e600c",
+    "url": "/static/js/main.340940e3.chunk.js"
+  },
+  {
+    "revision": "de8a3b301f78377b64aa",
+    "url": "/static/js/runtime-main.3256e4b4.js"
+  },
+  {
+    "revision": "1fdb3958af02053e4e0bbda02325e925",
+    "url": "/static/media/background.1fdb3958.jpg"
+  },
+  {
+    "revision": "c881c3c36765662e1a1a999f1bf78d89",
+    "url": "/static/media/logo small.c881c3c3.jpg"
+  }
+]);
