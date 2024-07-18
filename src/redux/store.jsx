d3cf10be3bd143fb9725ea41a03fa6364fb9c37c
moriya david <moriya1519@gmail.com>
2024-07-18 14:35:17 +0300

    update 18/07

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -13,7 +13,6 @@ export const reduser = combineReducers({
     UserOptionsReducer: UserOptionsReducer,
     ReferralsReducer: ReferralsReducer,
     userReducer: UserReducer,
-    referralsReducer: ReferralsReducer,
     userTypeReducer: UserTypeReducer,
     enamReducer: EnumReducer
 })
