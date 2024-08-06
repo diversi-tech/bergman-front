481e460f9596adc73b8395ce0970d67b888e8ac2
Rivki-Hayas <r0534164722@gmail.com>
2024-08-06 18:05:29 +0300

    a

--- src/axios/candidateOptionsAxios.jsx
+++ src/axios/candidateOptionsAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://bergman-back-2.onrender.com/api/CandidateOptions'; 
+const API_URL = 'http://localhost:8080/api/candidateOptions'; 
 
 const CandidateOptionsAxios = {
     
