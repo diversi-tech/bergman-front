dee3129a456f7b8c72e8727e8a5ddbb60fe4a199
moriya david <moriya1519@gmail.com>
2024-08-11 20:24:37 +0300

    company

--- src/axios/companyAxios.jsx
+++ src/axios/companyAxios.jsx
@@ -1,6 +1,62 @@
+// import axios from 'axios';
+
+// const API_URL = 'http://localhost:8080/api/companies'; // עדכן את ה-URL בהתאם לכתובת השרת שלך
+
+
+//  const copanyAxios ={
+//   getAll : async () => {
+//     try {
+//         const response = await axios.get(`${API_URL}`);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching :', error);
+//         throw error;
+//     }
+//   },
+//   getById : async (value) => {
+//     try {
+//         const response = await axios.get(`${API_URL}/${value}`);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching :', error);
+//         throw error;
+//     }
+//   },
+//   create : async (data) => {
+//     try {
+//         const response = await axios.post(`${API_URL}`,data);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching :', error);
+//         throw error;
+//     }
+//   },
+//   update : async (id) => {
+//     try {
+//         const response = await axios.put(`${API_URL}/${id}`);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching :', error);
+//         throw error;
+//     }
+//   },
+//   delete : async (id) => {
+//     try {
+//         const response = await axios.delete(`${API_URL}/${id}`);
+//         return response.data;
+//     } catch (error) {
+//         console.error('Error fetching :', error);
+//         throw error;
+//     }
+//   }
+//   }
+
+
+// export default copanyAxios;
+
 import axios from 'axios';
 
-const API_URL = 'https://localhost:8080/api/companies'; // עדכן את ה-URL בהתאם לכתובת השרת שלך
+const API_URL = 'http://localhost:8080/api/companies'; // עדכן את ה-URL בהתאם לכתובת השרת שלך
 
 
  const copanyAxios ={
@@ -53,8 +109,4 @@ const API_URL = 'https://localhost:8080/api/companies'; // עדכן את ה-URL 
 
 
 export default copanyAxios;
-
-
-
-
 
