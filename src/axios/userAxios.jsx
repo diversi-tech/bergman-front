28146663447ed115983f1ff5d1ae4110e5c1c4a6
Rivki-Hayas <r0534164722@gmail.com>
2024-08-11 06:56:37 +0300

    06:56

--- src/axios/userAxios.jsx
+++ src/axios/userAxios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import Cookies from 'js-cookie';
 
-const API_URL = 'http://localhost:8080/api/users'; // שים כאן את ה-URL שלך לקונטרולר
+const API_URL = 'https://bergman-back-2.onrender.com/api/users'; // שים כאן את ה-URL שלך לקונטרולר
 
 
 const UserAxios = {
@@ -13,9 +13,9 @@ const UserAxios = {
         const token = await response.data;
         Cookies.set("jwtToken", token, {
           sameSite: "None",
-          secure: true,
+          secure: true ,
           expires: 7
-        });
+      });
         // פענוח התוקן ושליפת המידע
         const decodedToken = jwtDecode(token);
         console.log("User Info from Token:", {
@@ -29,55 +29,64 @@ const UserAxios = {
         console.error("Invalid credentials");
       }
     } catch (error) {
-      return null;
-    }
+return  null;   }
   },
   getAllUsers: async () => {
-    try {
+    try 
+    {
       const response = await axios.get(`${API_URL}`);
       return response.data;
-    }
-    catch (error) {
+    } 
+    catch (error) 
+    {
       console.error('Error fetching users:', error);
       throw error;
     }
   },
   getAllManagers: async () => {
-    try {
+    try
+    {
       const response = await axios.get(`${API_URL}/managers`);
       return response.data;
-    }
-    catch (error) {
+    } 
+    catch (error) 
+    {
       console.error('Error fetching users:', error);
       throw error;
     }
   },
   addUser: async (user) => {
-    try {
+    try 
+    {
       const response = await axios.post(`${API_URL}`, user);
       return response.data;
-    }
-    catch (error) {
+    } 
+    catch (error)
+    {
       console.error('Error adding user:', error);
       throw error;
     }
   },
   deleteUser: async (userId) => {
-    try {
+    try
+    {
       const response = await axios.delete(`${API_URL}/${userId}`);
       return response.data;
-    }
-    catch (error) {
+    } 
+    catch (error) 
+    {
       console.error('Error deleting user:', error);
       throw error;
     }
   },
   updateUser: async (id, updatedUser) => {
-    try {
+    try 
+    {
       const response = await axios.put(`${API_URL}/${id}`, updatedUser);
       return response.data;
-    }
-    catch (error) {
+    } 
+    catch (error)
+    {
       console.error('Error updating user:', error);
       throw error;
     }
