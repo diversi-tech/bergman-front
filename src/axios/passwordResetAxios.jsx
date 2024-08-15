32c89956a99047989b2121bcd631f7b5fcd73b4b
Rivki-Hayas <r0534164722@gmail.com>
2024-08-15 04:34:30 +0300

    updat URL

--- src/axios/passwordResetAxios.jsx
+++ src/axios/passwordResetAxios.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://bergman-back.onrender.com/api';
+const API_URL = 'https://bergman-back-9cfv.onrender.com/api';
 
 export const requestPasswordReset = async (email) => {
   debugger
